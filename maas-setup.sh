4c99d192358b480d451cbde3df9b5e0b1c371961
Anton Smith <anton@huge.geek.nz>
2021-11-17 15:05:59 +0100

    added juju add-machine

--- maas-setup.sh
+++ maas-setup.sh
@@ -89,12 +89,32 @@ juju credentials
 juju bootstrap maas-cloud --bootstrap-constraints "tags=juju-controller mem=2G"
 
 # fire up the juju gui to view the fun
+# if it's a remote machine, you can use an SSH tunnel to get access to it:
+# e.g. ssh ubuntu@x.x.x.x -L8080:10.10.10.2:17070
 juju gui
 # get coffee
 
+# check jujus view of machines
+juju machines
+
+# add machines to juju from the maas cloud
+# it will grab the 3 we already created since they are in a "READY state"
+for ID in 1 2 3
+do
+    juju add-machine
+done
+
+# take a look at machines list again, should see 3 machines
+juju machines
+
+
 ### Ceph
+
+# deploy ceph-mon to LXD VMs inside our metal machines
 juju deploy -n 3 ceph-mon --to lxd:0,lxd:1,lxd:2
-juju deploy --config ceph-osd.yaml cs:ceph-osd -n 3 --to 0,1,2
+# deploy ceph-osd directly to the machines
+juju deploy --config maas-baremetal-k8s-tutorial/ceph-osd.yaml cs:ceph-osd -n 3 --to 0,1,2
+# relate ceph-mon and ceph-osd
 juju add-relation ceph-mon ceph-osd
 
 # watch the fun (with a another coffee). 
